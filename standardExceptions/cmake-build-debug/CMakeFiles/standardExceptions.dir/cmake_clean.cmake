0323267df9889a99a5824fdae99e9d61ea311038
ottermegazord <idaly.ali@live.fr>
2018-09-01 14:45:02 +0800

    Adds standardExceptions

---
+++ standardExceptions/cmake-build-debug/CMakeFiles/standardExceptions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/standardExceptions.dir/main.cpp.o"
+  "standardExceptions.pdb"
+  "standardExceptions"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/standardExceptions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
